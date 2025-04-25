703e7f7eb831ceea7a43c3bc7c24a4578a37321b
Jaysun <bobjason.legaspi@g.msuiit.edu.ph>
2025-04-25 11:46:20 +0800

    updated

--- slinked.c
+++ slinked.c
@@ -87,6 +87,107 @@ void extend_anywhere(struct node* head, const char *data, int pos){
     ptr->link = ptr2;
 }
 
+struct node* delete_head(struct node *head){
+
+    if(head == NULL){
+        printf("List is alreadty empty!");
+    }
+    else{
+        struct node *temp = head;
+        head = head->link;
+        free(temp);
+        
+        temp = NULL;
+    }
+
+    return head;
+}
+
+struct node* delete_tail1(struct node *head){
+
+    if(head == NULL){
+        printf("List is already empty!");
+    }
+    else if(head->link == NULL)
+    {
+        free(head);
+        head = NULL;
+    }
+    else{
+        struct node *temp = head;
+        struct node *temp2 = head;
+        while(temp->link != NULL){
+            temp2 = temp;
+            temp = temp->link;
+
+        }
+        temp2->link = NULL;
+        free(temp);
+        temp = NULL;
+    }
+    return head;
+}
+
+void delete_tail2(struct node *head)
+{
+    if(head == NULL){
+        printf("List is already empty!");
+    }
+    else if (head->link == NULL){
+
+        free(head);
+        head = NULL;
+    }else{
+        struct node *temp = head;
+        while(temp->link->link != NULL){
+            temp = temp->link;
+        }
+        free(temp->link);
+        temp->link = NULL;
+    }
+}
+
+void delete_position(struct node **head, int position){
+
+    struct node *current = *head;
+    struct node *previous = *head;
+
+    if(*head == NULL){
+        printf("List is already empty!");
+
+    }
+    else if (position == 1){
+
+        *head = current->link;
+        free(current);
+        current = NULL;
+    }else{
+
+        while(position != 1){
+            previous = current;
+            current = current->link;
+            position--;
+
+        }
+        previous->link = current->link;
+        free(current);
+        current = NULL;
+    }
+
+}
+
+struct node* delete_list(struct node *head){
+
+    struct node *temp = head;
+
+    while(temp != NULL){
+        temp = temp->link;
+        free(head);
+        head = temp;
+    }
+    return head;
+}
+
 
 int main(){
     struct node *head = malloc(sizeof(struct node));
@@ -110,18 +211,45 @@ int main(){
     head = extend_the_front(head, data);
     current = head;
 
-    int position = 4;
+    int position = 2;
 
     extend_anywhere(head, data, position);
 
     struct node *temp = head;
-    while (temp != NULL) {
-        printf("Name: %s\n", temp->data);
-        temp = temp->link;
-    }
+    print_the_data(head);
 
     count_the_nodes(head);
-   
+
+    head = delete_head(head);
+    current = head;
+
+    print_the_data(head);
+    count_the_nodes(head);
+
+    delete_tail1(head);
+
+
+    print_the_data(head);
+    count_the_nodes(head);
+
+    delete_tail2(head);
+
+    print_the_data(head);
+    count_the_nodes(head);
+
+    delete_position(&head, position);
+    current = head;
+
+    print_the_data(head);
+    count_the_nodes(head);
+
+    head = delete_list(head);
+    if(head == NULL)
+    {
+        printf("List is empty!");
+        return 0;
+    }
+
     temp = head;
     while (temp != NULL) {
         struct node *next = temp->link;
